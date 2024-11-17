131c3290ca0abe535bb8a4f7d59f25f3f71d2188
unknown <zhulieta.grudeva@gmail.com>
2024-11-18 00:36:31 +0200

    Added lines, words, and characters counters.

---
+++ Projects_C#/Count Lines, Words, and Characters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string filePath = "E:\\Sirma Academy\\Streams and Files\\Projects_C#\\Count Lines, Words, and Characters\\input.txt";
+
+        string[] lines = File.ReadAllLines(filePath);
+        int linesCount = lines.Length;
+        int wordsCount = 0;
+        int charsCount = 0;
+
+        foreach (string line in lines)
+        {
+            string[] words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            foreach (string word in words)
+            {
+                wordsCount++;
+            }
+            charsCount += line.Length;
+        }
+        Console.WriteLine($"Number of lines: {linesCount}");
+        Console.WriteLine($"Number of words: {wordsCount}");
+        Console.WriteLine($"Number of characters: {charsCount}");
+    }
+}
