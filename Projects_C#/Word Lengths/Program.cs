8beb92ba3e18a6c3ab999b8232cb8cb350374f0c
unknown <zhulieta.grudeva@gmail.com>
2024-11-17 14:07:10 +0200

    First excercise completed

---
+++ Projects_C#/Word Lengths/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string filePath = "E:\\Sirma Academy\\Streams and Files\\Projects\\Word Lengths\\input.txt";
+
+        using (StreamReader text = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = text.ReadLine()) != null)
+            {
+                string[] words = line.Split(' ');
+                foreach (string word in words)
+                {
+                    Console.Write($"{word.Length} ");
+                }
+            }
+        }
+
+    }
+}
