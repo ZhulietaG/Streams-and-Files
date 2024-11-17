b484d118abb7df34bccbaa5f29dcb41975322432
unknown <zhulieta.grudeva@gmail.com>
2024-11-17 16:32:02 +0200

    Second exercise completed

---
+++ Projects_C#/Character Frequency/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string filePath = "E:\\Sirma Academy\\Streams and Files\\Projects_C#\\Character Frequency\\input.txt";
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string input = reader.ReadToEnd();
+            input = Regex.Replace(input, @"\s+", "");
+            ////testing the new string without white spaces
+            //Console.WriteLine(input);
+            List<char> chars = new List<char>(input);
+            while (chars.Count > 0)
+            {
+                char curChar = chars[0];
+                int count = 1;
+                chars.RemoveAt(0);
+                for (int i = 0; i < chars.Count; i++)
+                {
+                    if (curChar == chars[i])
+                    {
+                        count++;
+                        chars.RemoveAt(i);
+                    }
+                }
+                Console.WriteLine($"{curChar}: {count}");
+            }
+            
+        }
+
+    }
+}
