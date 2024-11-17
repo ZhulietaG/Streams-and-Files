6994bd4e1151aad3e8533294ce8a07fa14098b2f
unknown <zhulieta.grudeva@gmail.com>
2024-11-17 20:49:35 +0200

    Third exercise completed

---
+++ Projects_C#/Replace Word/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string inputPath = "E:\\Sirma Academy\\Streams and Files\\Projects_C#\\Replace Word\\input.txt";
+        string outputPath = "E:\\Sirma Academy\\Streams and Files\\Projects_C#\\Replace Word\\output.txt";
+        Console.Write("Original word, which will be replaced: ");
+        string oldWord = Console.ReadLine();
+        Console.Write("New word for the replacement: ");
+        string newWord = Console.ReadLine();
+        string outputText;
+
+        using (StreamReader reader = new StreamReader(inputPath))
+        {
+            string text = reader.ReadToEnd();
+            outputText = text.Replace(oldWord, newWord);
+            string[] input = reader.ReadToEnd()
+                .Split(' ')
+                .ToArray();
+
+
+            File.WriteAllText(outputPath, outputText);
+        }
+    }
+}
