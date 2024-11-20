85059a48b9de54b20a51242e6c327fa04b329535
unknown <zhulieta.grudeva@gmail.com>
2024-11-20 15:41:00 +0200

    Added program for reversing each line in a file

---
+++ Projects_C#/Reverse Lines/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string inputPath = "E:\\Sirma Academy\\Streams and Files\\Projects_C#\\Reverse Lines\\input.txt";
+        string outputPath = "E:\\Sirma Academy\\Streams and Files\\Projects_C#\\Reverse Lines\\output.txt";
+        int linesCount = File.ReadAllLines(inputPath).Length;
+        string[] reversedLines = new string[linesCount];
+
+        using (StreamReader reader = new StreamReader(inputPath))
+        {
+            for (int i = 0; i < linesCount; i++)
+            {
+                string line = reader.ReadLine();
+                char[] chars = line.ToCharArray();
+                Array.Reverse(chars);
+                line = new string(chars);
+                reversedLines[i] = line;
+            }
+        }
+
+        using (StreamWriter writer = new StreamWriter(outputPath))
+        {
+            foreach (string line in reversedLines)
+            {
+                writer.WriteLine(line);
+            }
+        }
+
+    }
+}
+
