2687b697c9622b60ddaea35b3b2b507770c224c8
unknown <zhulieta.grudeva@gmail.com>
2024-11-20 22:31:08 +0200

    Added program that copies the content of a file to a new one.

---
+++ Projects_C#/Copy File Content/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string inputPath = "E:\\Sirma Academy\\Streams and Files\\Projects_C#\\Copy File Content\\input.txt";
+        string outputPath = "E:\\Sirma Academy\\Streams and Files\\Projects_C#\\Copy File Content\\output.txt";
+
+        using(StreamReader reader = new StreamReader(inputPath))
+        {
+            using (StreamWriter writer = new StreamWriter(outputPath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+        }
+    }
+}
