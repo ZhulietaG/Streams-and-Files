d95aebe95de2e3b504340d2c8dec3aef56929f13
unknown <zhulieta.grudeva@gmail.com>
2024-11-20 18:06:53 +0200

    Adding a program, which counts the number of unique words in a file.

---
+++ Projects_C#/Count Unique Words/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string filePath = "E:\\Sirma Academy\\Streams and Files\\Projects_C#\\Count Unique Words\\input.txt";
+
+        int result = 0;
+        HashSet<string> unique = new HashSet<string>();
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] words = line
+                .Split(new char[] { ' ', '\t', '.', ',', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string word in words)
+                {
+                    if (unique.Contains(word) == false)
+                    {
+                        unique.Add(word);
+                    }
+                }
+            }          
+            result = unique.Count();
+        }
+
+        Console.WriteLine("Unique words: " + result);
+    }
+}
