b59231cb039fb63efd07091588e8ca456c551c6d
unknown <zhulieta.grudeva@gmail.com>
2024-11-20 19:45:18 +0200

    Added program, which can merge lines from two files.

---
+++ Projects_C#/Merge Lines from Two Files/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string firstInPath= "E:\\Sirma Academy\\Streams and Files\\Projects_C#\\Merge Lines from Two Files\\input.txt";
+        string secondInPath = "E:\\Sirma Academy\\Streams and Files\\Projects_C#\\Merge Lines from Two Files\\input2.txt";
+        string outPath = "E:\\Sirma Academy\\Streams and Files\\Projects_C#\\Merge Lines from Two Files\\output.txt";
+
+        using (StreamWriter output = new StreamWriter(outPath))
+        {
+            using (StreamReader firstReader = new StreamReader(firstInPath))
+            {
+                string firstLine = firstReader.ReadLine();
+                using (StreamReader secondReader = new StreamReader(secondInPath))
+                {
+                    string secondLine = secondReader.ReadLine();
+
+                    while (firstLine != null || secondLine != null)
+                    {
+                        if (firstLine != null)
+                        {
+                            output.WriteLine(firstLine);
+                        }
+                        if (secondLine != null)
+                        {
+                            output.WriteLine(secondLine);
+                        }
+                        firstLine = firstReader.ReadLine();
+                        secondLine = secondReader.ReadLine();
+                    }
+
+                }
+            }
+        }
+    }
+}
