9a16b5627a51a6660ac95496926cbc26349e9a4b
unknown <zhulieta.grudeva@gmail.com>
2024-11-20 12:49:57 +0200

    Adding program for extracting a file name, extension, and size (in bytes).

---
+++ Projects_C#/Extract File/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string filePath = "E:\\Sirma Academy\\Streams and Files\\Projects_C#\\Extract File\\some_file.txt";
+
+        string fileName = Path.GetFileNameWithoutExtension(filePath);
+        string fileExtension = Path.GetExtension(filePath);
+        long fileSize = new FileInfo(filePath).Length;
+
+        Console.WriteLine("File Name: " + fileName);
+        Console.WriteLine("File Extension: " + fileExtension);
+        Console.WriteLine("File Size: " + fileSize);
+    }
+}
